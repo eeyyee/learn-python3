8f9621d761820ea0dd2feb587acfdedda5cd83a7
zhanghao <zhanghaozhanghao6@gmail.com>
2022-04-04 17:06:17 +0800

    day 01 && 02

---
+++ www/app.py
@@ -0,0 +1,9 @@
+import logging; logging.basicConfig(level=logging.INFO)
+from aiohttp import web
+
+def getIndex(request):
+    return web.Response(body="Hello World!")
+
+app = web.Application()
+app.add_routes([web.get("/", getIndex)])
+web.run_app(app, host="localhost", port=10090)
